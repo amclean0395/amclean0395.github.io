ecafd06f7fade4d84ccbb89f4332024cfce207ca
Andrew McLean <andrew.mclean.5895@gmail.com>
2023-02-14 18:26:02 -0800

    project  info, needs styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,13 +15,13 @@ function Home() {
             <div className="container">
 
                 <div>
-                <p>JavaScript</p>
-                <img style={{borderRadius: "10px"}} width="50px" height="50" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png" alt="error loading media"></img>
+                <p style={{width:"75px"}}>JavaScript</p>
+                <img style={{borderRadius: "10px"}} width="50px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png" alt="error loading media"></img>
                 </div>
 
                 <div>
-                <p style={{width:"75px"}} >React</p>
-                <img style={{borderRadius: "10px"}} width="50px" height="50" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="error loading media"></img>
+                <p style={{width:"75px"}}>React</p>
+                <img style={{borderRadius: "10px"}} width="50px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="error loading media"></img>
                 </div>
 
                 <div>
