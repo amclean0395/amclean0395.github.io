d3fdbec85c8decb7111bc0aaffc65affbb3d7456
Andrew McLean <andrew.mclean.5895@gmail.com>
2023-02-15 21:20:52 -0800

    change to hashrouter-again

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Education from "./Education";
 import Experience from "./Experience";
 import Home from "./Home";
